1e699e3467ce09a7e0fb2c44231a513635d18ad2
rachkor <rachael.korinek@gmail.com>
2015-03-15 21:33:51 -0700

    edit disqus

--- config/initializers/disqus_rails.rb
+++ config/initializers/disqus_rails.rb
@@ -10,3 +10,4 @@ DisqusRails.setup do |config|
     config::PUBLIC_KEY = "zxVspADpqCdcuLiHhzOr5B23CqG6QFsnihQkDn5sZudI4UiNR9rKhQGuCSapXnni" #leave blank if not used
     config::ACCESS_TOKEN = "9ef04c9bd392412ea9134ad17c6a120b"
   end
+end 
