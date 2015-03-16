94ccd3250d1a1630d614da23d5394696488f3d7b
rachkor <rachael.korinek@gmail.com>
2015-03-16 14:01:38 -0700

    edit initialize disqus params

--- config/initializers/disqus_rails.rb
+++ config/initializers/disqus_rails.rb
@@ -1,10 +1,12 @@
-DisqusRails.setup do |config|
-  if Rails.env == "development"
+if Rails.env == "development"
+  DisqusRails.setup do |config|
   	config::SHORT_NAME = "mendingme"
     config::SECRET_KEY = "7z3unB7rbZdCk4fbvzkkUFFJ2nPv7jpVZkBMY7YtvwZ08QoAfgKKYh6C8JtlEX6A" #leave blank if not used
     config::PUBLIC_KEY = "C4SzKATdvW6SbI6EqRZEu08yK8zoQ53YJld6oT7ztRxu4UjbLcq6VZQINS9eornY" #leave blank if not used
     config::ACCESS_TOKEN = "58b2dc32e7e747108eecd27ff5f090fe"
-  elsif Rails.env == "production"
+  end
+elsif Rails.env == "production"
+  DisqusRails.setup do |config|
   	config::SHORT_NAME = "mendingblogprod"
     config::SECRET_KEY = "6on1et5TMvE3lghcGaQGum8QQ7eyLVI8t5Z6PtQrLDGkicfYrbwKacktjeb9tohG" #leave blank if not used
     config::PUBLIC_KEY = "zxVspADpqCdcuLiHhzOr5B23CqG6QFsnihQkDn5sZudI4UiNR9rKhQGuCSapXnni" #leave blank if not used
