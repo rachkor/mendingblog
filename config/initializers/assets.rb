48e6bb971a46bee762683dddf59f538edca9fc17
rachkor <rachael.korinek@gmail.com>
2015-03-17 21:44:38 -0700

    edited monologue layouts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,9 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.woff )
+Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.eot )
+Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.ttf )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get "/about", to: "pages#about"
 
-  mount Monologue::Engine, at: '/'
+  mount Monologue::Engine, at: '/blog'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
